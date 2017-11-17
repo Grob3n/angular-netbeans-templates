88d2b4b911e1444bd682ab7de80ffb4120b00ac3
bdemasi <bdemasi@ENOVACOM-BDEM.dev.enovacom.fr>
2017-11-17 15:57:24 +0100

    ES Angular Templates

---
+++ src/org/templates/componentES/templates/angularComponentES.js
@@ -0,0 +1,12 @@
+// @author ${user}
+// @date ${date}
+import template from './${name}.html';
+
+export const ${name?replace("-", " ")?capitalize?replace(" ", "")?uncap_first?replace("Component", "")} = {
+	template: template,
+	controller: ${name?replace("-", " ")?capitalize?replace(" ", "")?uncap_first?replace("Component", "")}CntlFunc
+};
+
+function ${name?replace("-", " ")?capitalize?replace(" ", "")?uncap_first?replace("Component", "")}CntlFunc() {
+	
+};
